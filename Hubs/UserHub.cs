c342694ee43be407678a948def043eafe257af89
Dharmesh Singh <dharmeshsingh@live.com>
2023-07-12 09:54:20 +0530

    Telemetry

--- Hubs/UserHub.cs
+++ Hubs/UserHub.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.AspNetCore.SignalR;
+using Microsoft.Extensions.Logging;
+using Microsoft.ApplicationInsights;
+using static Microsoft.ApplicationInsights.MetricDimensionNames.TelemetryContext;
 
 namespace SignalRProject.Hubs
 {
@@ -6,14 +9,40 @@ namespace SignalRProject.Hubs
     {
         public static int TotalViews { get; set; }
         public static int TotalUsers { get; set; }
+        private readonly ILogger<UserHub> _logger;
+        private readonly TelemetryClient _telemetryClient;
+        public UserHub(ILogger<UserHub> logger, TelemetryClient telemetryClient)
+        {
+            _logger = logger;
+            _telemetryClient = telemetryClient;
+        }
         public async Task NewWindowLoaded()
         {
+            // Track a custom event
+            _telemetryClient.TrackEvent("MessageSent", new Dictionary<string, string>
+            {
+                { "User", TotalViews.ToString() },
+                { "Message", TotalViews.ToString() }
+            });
+            
+
+            // Log information
+            _logger.LogInformation("Message sent by {User}: {Message}", TotalViews, TotalViews);
+
             TotalViews++;
             await Clients.All.SendAsync("updateTotalVies", TotalViews);
         }
         public override Task OnConnectedAsync()
         {
             TotalUsers++;
+            _telemetryClient.TrackEvent("MessageSent-OnConnectedAsync-TotalUsers", new Dictionary<string, string>
+            {
+                { "User", TotalUsers.ToString() },
+                { "Message", "OnConnectedAsync" }
+            });
+
+            // Log information
+            _logger.LogInformation("Message sent by {User}: {Message}", TotalUsers, OnConnectedAsync);
             Clients.All.SendAsync("updateTotalUsers",TotalUsers).GetAwaiter().GetResult();
             return base.OnConnectedAsync();
            
--- Program.cs
+++ Program.cs
@@ -15,6 +15,7 @@ builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.Requ
     .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddControllersWithViews();
 builder.Services.AddSignalR();
+builder.Services.AddApplicationInsightsTelemetry();
 
 var app = builder.Build();
 
